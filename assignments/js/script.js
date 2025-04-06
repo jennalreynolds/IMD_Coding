dc855966846102268a2faf6168685469e6bdd4c9
jenna.reynolds <jenna.reynolds819@gmail.com>
2025-04-06 18:59:31 -0500

    redo4/6

---
+++ assignments/js/script.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.getElementById("toggle-button");
+const navListItems = document.getElementById("nav-menu-list");
+
+toggleBtn.addEventListener("click", function () {
+  navListItems.classList.toggle("active");
+});
